ae638c7fd72cc87fe0b25cc3205b6db770658853
Eddie Saboia <eddie@mobills.com.br>
2020-06-22 19:09:53 -0300

    a

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const routes = express.Router();
 
-
+//https://github.com/EddieSaboia/MaquinaNorma
+//https://github.com/JonathanPonte/MaquinaNorma Font do meu amigo la
 routes.get("/caminhoURL", (request, response) => {
  
   return response.json({
